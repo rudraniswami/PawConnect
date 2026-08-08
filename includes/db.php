54d50253108cc4c0f996b895c3c1a53db140a59c
rudrani swami <rudraniswami852@gmail.com>
2026-08-08 15:36:33 +0530

    First commit

---
+++ includes/db.php
@@ -0,0 +1,17 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "pawconnect";
+
+$conn = mysqli_connect($host, $username, $password, $database);
+
+if (!$conn) {
+    die("Database connection failed: " . mysqli_connect_error());
+   
+}
+else{
+    echo "Hello";
+}
+?>
