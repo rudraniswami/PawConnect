ebd81e49d593926850f311e3e67f15b70e169d7b
monalibhelke <monalibhelke16@gmail.com>
2026-08-20 17:09:18 +0530

    Update complete_adoption.php

--- complete_adoption.php
+++ complete_adoption.php
@@ -1,3 +1,30 @@
+<?php
+include "db.php";
+
+$id = isset($_GET['id']) ? intval($_GET['id']) : 0;
+
+$completed_adoption = null;
+
+if ($id > 0) {
+
+    $update = "UPDATE adoption_requests 
+               SET status = 'Completed'
+               WHERE id = $id";
+
+    mysqli_query($conn, $update);
+
+    $query = "SELECT * FROM adoption_requests
+              WHERE id = $id
+              AND status = 'Completed'";
+
+    $result = mysqli_query($conn, $query);
+
+    if ($result && mysqli_num_rows($result) > 0) {
+        $completed_adoption = mysqli_fetch_assoc($result);
+    }
+}
+?>
+
 <div style="
     background:#ffffff;
     border:3px solid #173c2d;
